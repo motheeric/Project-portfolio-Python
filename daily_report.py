c36c162f11566e848f215d1cd472fcc641ba247b
motheeric <mothe.eric@gmail.com>
2025-11-20 16:10:38 +0100

    V1

--- daily_report.py
+++ daily_report.py
@@ -1,1 +1,14 @@
+import data_handler as dh
+import portfolio as pf
+
+df_prices = dh.get_initial_data(dh.symbols)
+weights = [0.1]*10
+cumulative_value, port_returns = pf.calc_portfolio(df_prices, weights)
+metrics = pf.portfolio_metrics(cumulative_value, port_returns)
+corr = pf.correlation_matrix(df_prices)
+
+# Sauvegarde CSV
+metrics_df = pd.DataFrame([metrics])
+metrics_df.to_csv("daily_metrics.csv")
+corr.to_csv("daily_correlation.csv")
 
