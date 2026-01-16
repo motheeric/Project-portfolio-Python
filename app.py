7fbe9366b9b81ccd7d92602c5218a845efe182de
Leanapaseuth <lea.napaseuth@edu.devinci.fr>
2026-01-16 19:16:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Portfolio", layout="wide")
+choice = st.sidebar.radio("Choisir :", ["Quant A", "Quant B"])
+
+if choice == "Quant A":
+    import code.py
+else:
+    import dashboard.py
 
