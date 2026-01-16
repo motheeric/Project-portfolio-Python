c6da3b4acc01715cab6a76e035eb07c7ff78bd42
Leanapaseuth <lea.napaseuth@edu.devinci.fr>
2026-01-16 20:04:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ st.set_page_config(page_title="Portfolio", layout="wide")
 
 BASE = Path(__file__).resolve().parent
 
-choice = st.sidebar.radio("Choisir :", ["Quant A", "Quant B"])
+choice = st.sidebar.radio("Choisir :", ["QuantA", "QuantB"])
 
-if choice == "Quant A":
-    runpy.run_path(str(BASE / "quant A" / "code.py"), run_name="__main__")
+if choice == "QuantA":
+    runpy.run_path(str(BASE / "quantA" / "code.py"), run_name="__main__")
 else:
-    runpy.run_path(str(BASE / "quant B" / "dashboard.py"), run_name="__main__")
+    runpy.run_path(str(BASE / "quantB" / "dashboard.py"), run_name="__main__")
