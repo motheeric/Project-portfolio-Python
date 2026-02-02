bfb7283b8c7514e1e09778f1905954886b529b46
motheeric <mothe.eric@gmail.com>
2026-02-02 16:10:30 +0100

    Update dashboard.py

--- quantB/dashboard.py
+++ quantB/dashboard.py
@@ -7,26 +7,25 @@ import time
 
 st.title("Dashboard Quant B - Multi-Asset Portfolio")
 
-# Charger les données initiales
+# Loading initial data
 df_prices = dh.get_initial_data(dh.symbols)
 
-# Rafraîchissement toutes les 5 minutes
+# Refresh every 5min
 st_autorefresh_interval = 300
 last_refresh = st.session_state.get("last_refresh", 0)
 if time.time() - last_refresh > st_autorefresh_interval:
     st.session_state.last_refresh = time.time()
     st.rerun() 
 
-# Mise à jour des nouvelles données
+# update the data
 df_prices = dh.update_data(df_prices, dh.symbols)
 
-# --- SYNCHRONISATION DES POIDS ET DES ACTIFS ---
+# synchronize weights 
 available_symbols = df_prices.columns.tolist()
 
 weights = []
 st.sidebar.header("Paramètres du portefeuille")
 
-# On boucle sur les colonnes réelles présentes pour éviter l'erreur de "shape mismatch"
 for sym in available_symbols:
     w = st.sidebar.slider(f"Poids {sym}", 0.0, 1.0, 0.1)
     weights.append(w)
@@ -39,29 +38,29 @@ if len(weights) > 0:
     else:
         weights = np.ones(len(weights)) / len(weights)
 else:
-    st.error("Aucune donnée disponible pour les symboles sélectionnés.")
+    st.error("No available data")
     st.stop()
 
-# Calcul du portefeuille
+# Portfolio calculations
 cumulative_value, port_returns = pf.calc_portfolio(df_prices, weights)
 
 if cumulative_value is None:
-    st.warning("Pas assez de données pour calculer le portefeuille.")
+    st.warning("not enough data.")
     st.stop()
 
 metrics = pf.portfolio_metrics(cumulative_value, port_returns)
 corr = pf.correlation_matrix(df_prices)
 
-# Graphiques
-st.subheader("Valeur cumulée du portefeuille")
-fig_portfolio = px.line(cumulative_value, title="Portefeuille cumulatif")
+# Graphics
+st.subheader("cumulative value of the portfolio")
+fig_portfolio = px.line(cumulative_value, title="Portfolio value")
 st.plotly_chart(fig_portfolio)
 
-st.subheader("Prix des actifs")
-fig_assets = px.line(df_prices, title="Prix des actifs disponibles")
+st.subheader("assets prices")
+fig_assets = px.line(df_prices, title="assets prices ")
 st.plotly_chart(fig_assets)
 
-st.subheader("Matrice de corrélation")
+st.subheader("correlation matrice")
 st.dataframe(corr)
 
 st.subheader("KPIs")
